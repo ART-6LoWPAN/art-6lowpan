5709c6f2b847ec3e0a676bf2545149bbe8bebe9b
armink <armink.ztl@gmail.com>
2017-12-08 22:43:56 +0800

    1、【开启】POSIX 选型支持。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- firmware/app/rtconfig.h
+++ firmware/app/rtconfig.h
@@ -142,7 +142,9 @@
 
 #define RT_USING_LIBC
 /* RT_USING_PTHREADS is not set */
-/* RT_USING_POSIX is not set */
+#define RT_USING_POSIX
+/* RT_USING_POSIX_MMAP is not set */
+/* RT_USING_POSIX_TERMIOS is not set */
 
 /* Network stack */
 
