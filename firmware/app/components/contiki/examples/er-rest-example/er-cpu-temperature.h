13b63febf29ad94b1713a5ccdb47e28536ae94e8
xidongxu <xi_dongxu@126.com>
2017-12-08 22:30:34 +0800

    1.【添加】 添加获取cpu使用率与获取cpu温度的coap资源文件； 2.【修改】 修改coap服务器例程，开启获取CPU温度与使用率的资源； 3.【添加】 添加获取cpu温度的驱动文件；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

---
+++ firmware/app/components/contiki/examples/er-rest-example/er-cpu-temperature.h
@@ -0,0 +1,14 @@
+/*
+ * er-cpu-temperature.h
+ *
+ *  Created on: 2017Äê12ÔÂ6ÈÕ
+ *      Author: xdx
+ */
+
+#ifndef _ER_CPU_TEMPERATURE_H_
+#define _ER_CPU_TEMPERATURE_H_
+
+void get_cpu_temperature_init(void);
+uint32_t get_cpu_temperature(void);
+
+#endif /* _ER_CPU_TEMPERATURE_H_ */
